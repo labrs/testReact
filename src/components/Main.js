8a95bbf6f415130103ce6507e458fed8d0719a4c
labrs <iamsudipto@gmail.com>
2021-06-07 00:25:40 +0200

    React Component framework

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Welcome from './Welcome';
+import EInfo from './EInfo';
+
+const Main = () => {
+    return (
+      <main id="wrapper">
+      <Welcome />
+      <EInfo />
+ 
+      </main>
+    );
+}
+
+export default Main;
